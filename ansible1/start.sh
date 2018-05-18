1f3011e0f76dbb32ab454ce53975527488fa7531
Kedra, Jurek <Jerzy.Kedra@sabre.com>
2018-05-18 19:19:31 +0200

    updates

--- ansible1/start.sh
+++ ansible1/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker build -t jkedra/ansible .
-docker container run --rm -v `pwd`:`pwd` -w `pwd` -ti jkedra/ansible
+docker container run --rm -v `pwd`:`pwd` -w `pwd` -ti -h ansible jkedra/ansible
